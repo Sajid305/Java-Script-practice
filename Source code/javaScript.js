d4cc8fb5ae2fcc1d4fdfcdba2543c54d107dc43d
Sajid305 <mdshajid305@gmail.com>
2018-11-04 03:47:05 +0600

    js practice nothing fancy

---
+++ Source code/javaScript.js
@@ -0,0 +1,15 @@
+
+// var egg_price = 12;
+// var moreEgg = 12;
+// var totalEgg = egg_price*moreEgg;
+// console.log(totalEgg)
+
+// const pi = 3.14159;
+// var radius = 7;
+// var totalArea = pi * radius**2;
+// console.log('area of circle is',totalArea,'inc')
+
+var usd_to_bdt = 83.5;
+var bdt = 5000;
+var usd = bdt / usd_to_bdt;
+console.log(usd)
