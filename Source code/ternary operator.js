8956aa5c793693802dc3a614e0a43f8482ae448f
Sajid305 <mdshajid305@gmail.com>
2018-11-09 22:14:31 +0600

    Ternary Operator

---
+++ Source code/ternary operator.js
@@ -0,0 +1,27 @@
+
+var number = 18;
+var result;
+var reminder = number % 2;
+
+
+// if (0 == reminder ){
+//     result = ('this number is even')
+// }else{                                   # we can use ternary operator for this
+//     result = ('this number is odd')
+// }
+// console.log(result)
+
+                    
+                                    // Odd even finder programme with ternary operator
+
+result = (0==reminder) ? 'even' : 'odd';
+console.log('your number is',result);
+
+         
+                                // negative positive finder using yernary operator
+
+var n = -11;
+var it_is;
+
+result2 = (n<0) ? 'negative' : 'positive';
+console.log(result2)
